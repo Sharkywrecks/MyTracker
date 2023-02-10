5e254705d6f784c99e6125a72ef51612fe52ecd6
james <jamesmedley00@gmail.com>
2023-02-10 10:47:17 +0000

    Added AddFoodActivity

---
+++ app/src/main/java/com/TrackManInc/mytracker/AddFoodActivity.java
@@ -0,0 +1,14 @@
+package com.TrackManInc.mytracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food);
+    }
+}
