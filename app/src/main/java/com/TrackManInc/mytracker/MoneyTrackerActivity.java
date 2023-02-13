fc077564d5f92c9830e2ede420ce24b66a8035b9
james <jamesmedley00@gmail.com>
2023-02-13 22:46:34 +0000

    MoneyTrackerActivity commit. Converted to BarChart. Added Axis Labels and improved chart UI settings. Fixed minor issues with XML.

--- app/src/main/java/com/TrackManInc/mytracker/MoneyTrackerActivity.java
+++ app/src/main/java/com/TrackManInc/mytracker/MoneyTrackerActivity.java
@@ -6,21 +6,21 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
-import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import java.util.ArrayList;
 
 public class MoneyTrackerActivity extends AppCompatActivity {
 
     private RadioButton weekRadioButton, monthRadioButton, yearRadioButton;
-    private LineChart lineChart;
+    private BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,25 +34,39 @@ public class MoneyTrackerActivity extends AppCompatActivity {
         weekRadioButton = findViewById(R.id.week_radio_btn);
         monthRadioButton = findViewById(R.id.month_radio_btn);
         yearRadioButton = findViewById(R.id.year_radio_btn);
-        lineChart = findViewById(R.id.line_chart);
+        barChart = findViewById(R.id.bar_chart);
         graphSettings();
+        weekRadioButton.setChecked(true);
         generateGraph(R.id.week_radio_btn); // defaults to week graph
     }
 
     private void graphSettings(){  // temporary settings for now
-        lineChart.setBackgroundColor(Color.WHITE);
-        lineChart.setNoDataText("No Data.");
-        lineChart.setNoDataTextColor(Color.BLACK);
-        lineChart.setDrawGridBackground(true);
-        lineChart.setDrawBorders(true);
-        lineChart.setBorderColor(Color.BLACK);
-        lineChart.setBorderWidth(3);
+        barChart.setNoDataText("No Data.");
+        barChart.setBackgroundColor(Color.WHITE);
+        barChart.setNoDataTextColor(Color.BLACK);
+        barChart.setBorderColor(Color.BLACK);
+        barChart.setDrawBorders(true);
+        barChart.setBorderWidth(3);
+
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        barChart.getAxisLeft().setDrawGridLines(true);
+        barChart.getAxisRight().setDrawGridLines(true);
+        barChart.getAxisLeft().setDrawLabels(true);
+        barChart.getAxisRight().setDrawLabels(true);
+
+        barChart.setTouchEnabled(true);
+        barChart.setPinchZoom(false);
+        barChart.setScaleEnabled(false);
+        barChart.setDragEnabled(true);
 
         Description chartDescription = new Description();
         chartDescription.setText("Money Tracker");
         chartDescription.setTextColor(Color.BLACK);
         chartDescription.setTextSize(20);
-        lineChart.setDescription(chartDescription);
+        chartDescription.setEnabled(false); // hidden
+        barChart.setDescription(chartDescription);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -64,72 +78,80 @@ public class MoneyTrackerActivity extends AppCompatActivity {
             case R.id.week_radio_btn:
                 monthRadioButton.setChecked(false);
                 yearRadioButton.setChecked(false);
-                LineDataSet weekData = findWeekData();
+                BarDataSet weekData = findWeekData();
                 addDataToGraph(weekData);
                 break;
             case R.id.month_radio_btn:
                 weekRadioButton.setChecked(false);
                 yearRadioButton.setChecked(false);
-                LineDataSet monthData = findMonthData();
+                BarDataSet monthData = findMonthData();
                 addDataToGraph(monthData);
                 break;
             case R.id.year_radio_btn:
                 weekRadioButton.setChecked(false);
                 monthRadioButton.setChecked(false);
-                LineDataSet yearData = findYearData();
+                BarDataSet yearData = findYearData();
                 addDataToGraph(yearData);
                 break;
         }
     }
 
-    private void addDataToGraph(LineDataSet lineDataSet){
-        ArrayList<ILineDataSet> data = new ArrayList<>();
-        data.add(lineDataSet);
-        LineData linedata = new LineData(data);
-        lineChart.setData(linedata);
-        lineChart.invalidate();
+    private void addDataToGraph(BarDataSet barDataSet){
+        BarData barData = new BarData();
+        barData.addDataSet(barDataSet);
+        barChart.setData(barData);
+        barChart.invalidate();
     }
 
-    private LineDataSet findWeekData(){ // will eventually add real data from db instead
-        ArrayList<Entry> testData = new ArrayList<>();
-        testData.add(new Entry(1,2));
-        testData.add(new Entry(2,3));
-        testData.add(new Entry(3,4));
-        testData.add(new Entry(4,24));
-        testData.add(new Entry(5,6));
-        testData.add(new Entry(6,7));
-        testData.add(new Entry(7,8));
-        LineDataSet lineDataSet = new LineDataSet(testData, "Test Data Set 1");
-        lineDataSet.setLineWidth(4);
-        return lineDataSet;
+    private BarDataSet findWeekData(){ // will eventually add real data from db instead
+        final String[] daysOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        barChart.getXAxis().setLabelCount(daysOfWeek.length);
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(daysOfWeek));
+
+        ArrayList<BarEntry> testData = new ArrayList<>();
+        testData.add(new BarEntry(0,2)); // x value must start with index 0
+        testData.add(new BarEntry(1,3));
+        testData.add(new BarEntry(2,4));
+        testData.add(new BarEntry(3,24));
+        testData.add(new BarEntry(4,6));
+        testData.add(new BarEntry(5,7));
+        testData.add(new BarEntry(6,8));
+        BarDataSet barDataSet = new BarDataSet(testData, "Test Data Set 1");
+        return barDataSet;
     }
 
-    private LineDataSet findMonthData(){ // will eventually add real data from db instead
-        ArrayList<Entry> testData = new ArrayList<>();
-        testData.add(new Entry(1,2));
-        testData.add(new Entry(2,3));
-        testData.add(new Entry(3,4));
-        testData.add(new Entry(4,5));
-        testData.add(new Entry(5,16));
-        testData.add(new Entry(6,7));
-        testData.add(new Entry(7,8));
-        LineDataSet lineDataSet = new LineDataSet(testData, "Test Data Set 2");
-        lineDataSet.setLineWidth(4);
-        return lineDataSet;
+    private BarDataSet findMonthData(){ // will eventually add real data from db instead
+        ArrayList<BarEntry> testData = new ArrayList<>();
+        testData.add(new BarEntry(1,2));
+        testData.add(new BarEntry(2,3));
+        testData.add(new BarEntry(3,4));
+        testData.add(new BarEntry(4,24));
+        testData.add(new BarEntry(5,6));
+        testData.add(new BarEntry(6,7));
+        testData.add(new BarEntry(7,8));
+        BarDataSet barDataSet = new BarDataSet(testData, "Test Data Set 2");
+        return barDataSet;
     }
 
-    private LineDataSet findYearData(){ // will eventually add real data from db instead
-        ArrayList<Entry> testData = new ArrayList<>();
-        testData.add(new Entry(1,2));
-        testData.add(new Entry(2,10));
-        testData.add(new Entry(3,4));
-        testData.add(new Entry(4,5));
-        testData.add(new Entry(5,6));
-        testData.add(new Entry(6,7));
-        testData.add(new Entry(7,8));
-        LineDataSet lineDataSet = new LineDataSet(testData, "Test Data Set 3");
-        lineDataSet.setLineWidth(4);
-        return lineDataSet;
+    private BarDataSet findYearData(){ // will eventually add real data from db instead
+        final String[] months = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        barChart.getXAxis().setLabelCount(months.length);
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(months));
+        ArrayList<BarEntry> testData = new ArrayList<>();
+        testData.add(new BarEntry(0,2)); // x value must start with index 0
+        testData.add(new BarEntry(1,2));
+        testData.add(new BarEntry(2,3));
+        testData.add(new BarEntry(3,4));
+        testData.add(new BarEntry(4,24));
+        testData.add(new BarEntry(5,6));
+        testData.add(new BarEntry(6,7));
+        testData.add(new BarEntry(7,8));
+        testData.add(new BarEntry(8,2));
+        testData.add(new BarEntry(9,3));
+        testData.add(new BarEntry(10,4));
+        testData.add(new BarEntry(11,24));
+        BarDataSet barDataSet = new BarDataSet(testData, "Test Data Set 3");
+        return barDataSet;
     }
 
 }
