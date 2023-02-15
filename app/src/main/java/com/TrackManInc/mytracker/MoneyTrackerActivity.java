cea7972c2a6c87e554eaf2a4f6c9b99c4d451280
James Medley <81381425+jmed13@users.noreply.github.com>
2023-02-15 11:49:27 +0000

    Updated graphs to display data up to the current date.

--- app/src/main/java/com/TrackManInc/mytracker/MoneyTrackerActivity.java
+++ app/src/main/java/com/TrackManInc/mytracker/MoneyTrackerActivity.java
@@ -16,7 +16,11 @@ import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Random;
 
 public class MoneyTrackerActivity extends AppCompatActivity {
@@ -32,13 +36,13 @@ public class MoneyTrackerActivity extends AppCompatActivity {
     }
 
     private void setupUIView() {
-        weekRadioButton = findViewById(R.id.week_radio_btn);
-        monthRadioButton = findViewById(R.id.month_radio_btn);
-        yearRadioButton = findViewById(R.id.year_radio_btn);
+        weekRadioButton = findViewById(R.id.prev_week_radio_btn);
+        monthRadioButton = findViewById(R.id.prev_month_radio_btn);
+        yearRadioButton = findViewById(R.id.prev_year_radio_btn);
         barChart = findViewById(R.id.bar_chart);
         graphSettings();
         weekRadioButton.setChecked(true);
-        generateGraph(R.id.week_radio_btn); // defaults to week graph
+        generateGraph(R.id.prev_week_radio_btn); // defaults to week graph
     }
 
     private void graphSettings(){  // temporary settings for now
@@ -65,13 +69,6 @@ public class MoneyTrackerActivity extends AppCompatActivity {
         barChart.setPinchZoom(false);
         barChart.setScaleEnabled(false);
         barChart.setDragEnabled(true);
-
-        Description chartDescription = new Description();
-        chartDescription.setText("Money Tracker");
-        chartDescription.setTextColor(Color.BLACK);
-        chartDescription.setTextSize(20);
-        chartDescription.setEnabled(false); // hidden
-        barChart.setDescription(chartDescription);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -80,19 +77,19 @@ public class MoneyTrackerActivity extends AppCompatActivity {
 
     private void generateGraph(int id) {
         switch (id){
-            case R.id.week_radio_btn:
+            case R.id.prev_week_radio_btn:
                 monthRadioButton.setChecked(false);
                 yearRadioButton.setChecked(false);
                 BarDataSet weekData = findWeekData();
                 addDataToGraph(weekData);
                 break;
-            case R.id.month_radio_btn:
+            case R.id.prev_month_radio_btn:
                 weekRadioButton.setChecked(false);
                 yearRadioButton.setChecked(false);
                 BarDataSet monthData = findMonthData();
                 addDataToGraph(monthData);
                 break;
-            case R.id.year_radio_btn:
+            case R.id.prev_year_radio_btn:
                 weekRadioButton.setChecked(false);
                 monthRadioButton.setChecked(false);
                 BarDataSet yearData = findYearData();
@@ -100,6 +97,16 @@ public class MoneyTrackerActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    private void addChartDescription(String description){
+        Description chartDescription = new Description();
+        chartDescription.setText(description);
+        chartDescription.setTextColor(Color.BLACK);
+        chartDescription.setTextSize(20);
+        chartDescription.setEnabled(true);
+        barChart.setDescription(chartDescription);
+    }
+
 
     private void addDataToGraph(BarDataSet barDataSet){
         BarData barData = new BarData();
@@ -109,41 +116,72 @@ public class MoneyTrackerActivity extends AppCompatActivity {
         barChart.animateXY(500, 900, Easing.Linear, Easing.EaseOutCubic);
     }
 
-    private BarDataSet findWeekData(){ // will eventually add real data from db instead
+    private BarDataSet findWeekData(){ // previous 7 days
         final String[] daysOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-        barChart.getXAxis().setLabelCount(daysOfWeek.length);
-        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(daysOfWeek));
-
+        final int length = daysOfWeek.length;
+        String[] shiftedDays = new String[length];
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK) - 2;
+        for(int i = 0; i<length-(day+1);i++){
+            shiftedDays[i] = daysOfWeek[day+1+i];
+        }
+        int count = 0;
+        for(int i = length-(day+1); i<length;i++){
+            shiftedDays[i] = daysOfWeek[count];
+            count++;
+        }
         ArrayList<BarEntry> testData = new ArrayList<>();
-        for(int dayOfWeek = 0; dayOfWeek<7; dayOfWeek++){
+        for(int dayOfWeek = 0; dayOfWeek<length; dayOfWeek++){
             testData.add(new BarEntry(dayOfWeek, new Random().nextInt(20)));
         }
-
+        barChart.getXAxis().setLabelCount(shiftedDays.length);
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(shiftedDays));
+        addChartDescription("Previous 7 Days");
         return new BarDataSet(testData, "Test Data Set 1");
     }
 
-    private BarDataSet findMonthData(){ // will eventually add real data from db instead
+    private BarDataSet findMonthData() { // Previous 4 weeks
+        String[] weekStartDates = new String[4];
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM", Locale.ENGLISH);
+        for(int i=3;i>=0;i--){
+            cal.add(Calendar.DATE, -7);
+            Date weekStartDate = cal.getTime();
+            weekStartDates[i] = formatter.format(weekStartDate);
+        }
         ArrayList<BarEntry> testData = new ArrayList<>();
-        testData.add(new BarEntry(1,2));
-        testData.add(new BarEntry(2,3));
-        testData.add(new BarEntry(3,4));
-        testData.add(new BarEntry(4,24));
-        testData.add(new BarEntry(5,6));
-        testData.add(new BarEntry(6,7));
-        testData.add(new BarEntry(7,8));
+        for(int dayOfWeek = 0; dayOfWeek<4; dayOfWeek++){
+            testData.add(new BarEntry(dayOfWeek, new Random().nextInt(20)));
+        }
+        barChart.getXAxis().setLabelCount(weekStartDates.length);
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(weekStartDates));
+        addChartDescription("Previous 4 Weeks");
         return new BarDataSet(testData, "Test Data Set 2");
     }
 
-    private BarDataSet findYearData(){ // will eventually add real data from db instead
+    private BarDataSet findYearData(){ // previous 12 months
         final String[] months = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-        barChart.getXAxis().setLabelCount(months.length);
-        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(months));
-        ArrayList<BarEntry> testData = new ArrayList<>();
-
-        for(int month = 0; month<12; month++){
-            testData.add(new BarEntry(month,new Random().nextInt(20)));
+        final int length = months.length;
+        String[] shiftedMonths = new String[length];
+        Calendar calendar = Calendar.getInstance();
+        int monthNum = calendar.get(Calendar.MONTH);
+        for(int i = 0; i<length-(monthNum+1);i++){
+            shiftedMonths[i] = months[monthNum+1+i];
         }
-
+        int count = 0;
+        for(int i = length-(monthNum+1); i<length;i++){
+            shiftedMonths[i] = months[count];
+            count++;
+        }
+        ArrayList<BarEntry> testData = new ArrayList<>();
+        for(int month = 0; month<length; month++){
+            testData.add(new BarEntry(month, new Random().nextInt(20)));
+        }
+        barChart.getXAxis().setLabelCount(shiftedMonths.length);
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(shiftedMonths));
+        addChartDescription("Previous 7 Days");
+        addChartDescription("Previous 12 Months");
         return new BarDataSet(testData, "Test Data Set 3");
     }
 
