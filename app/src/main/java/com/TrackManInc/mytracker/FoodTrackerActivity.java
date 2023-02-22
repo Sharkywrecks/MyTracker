63e9a51905b02df0b40c4168078cc45a647912ee
dan <danjlaw28@gmail.com>
2023-02-22 17:43:22 +0000

    food tracker layout

--- app/src/main/java/com/TrackManInc/mytracker/FoodTrackerActivity.java
+++ app/src/main/java/com/TrackManInc/mytracker/FoodTrackerActivity.java
@@ -3,13 +3,29 @@ package com.TrackManInc.mytracker;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.TrackManInc.mytracker.Model.Nutrients;
+import com.TrackManInc.mytracker.Model.Users;
+import com.TrackManInc.mytracker.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class FoodTrackerActivity extends AppCompatActivity {
 
-    private ProgressBar calorieBar, proteinBar, carbsBar;
-    private Integer calorieVal, proteinVal, carbsVal, calorieTarget, proteinTarget, carbsTarget;
+    private ProgressBar calorieBar, proteinBar, carbsBar, fibreBar, saltBar;
+    private int calorieVal=0;
+    private int proteinVal=0;
+    private int carbsVal=0;
+    private int fibreVal=0;
+    private int saltVal=0;
+    private int fatVal=0;
+    private int calorieTarget, proteinTarget, carbsTarget, fibreTarget, saltTarget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +33,7 @@ public class FoodTrackerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_food_tracker);
 
         //temp values//
-        calorieTarget = 2000;
+        calorieTarget = 2500;
         proteinTarget = 55;
         carbsTarget = 333;
         calorieVal = 1350;
@@ -42,6 +58,29 @@ public class FoodTrackerActivity extends AppCompatActivity {
 
     }
 
+    private String retrieveNutrients(String formattedDate) {
+        final String[] totalMoney = {""};
+        final DatabaseReference RootRef = FirebaseDatabase.getInstance().getReference();
+        final DatabaseReference nutrientRef = RootRef.child("User Foods").child(Prevalent.currentOnlineUser.getEmail()).child(formattedDate);
+        nutrientRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot nutrientDS:snapshot.getChildren()){
+                    Nutrients usersNutrients = nutrientDS.getValue(Nutrients.class);
+                    carbsVal+=Integer.parseInt(usersNutrients.getCarbohydrates());
+                    proteinVal += Integer.parseInt(usersNutrients.getProtein());
+                    fatVal += Integer.parseInt(usersNutrients.getFat());
+                    saltVal += Integer.parseInt(usersNutrients.getSalt());
+                    fibreVal += Integer.parseInt(usersNutrients.getFibre());
 
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        })
+    }
 
 }
---
+++ app/src/main/java/com/TrackManInc/mytracker/Model/Nutrients.java
@@ -0,0 +1,61 @@
+package com.TrackManInc.mytracker.Model;
+
+public class Nutrients {
+    private String carbs;
+    private String protein;
+
+    private String fat;
+    private String salt;
+    private String fibre;
+
+    public Nutrients() {
+    }
+
+    public Nutrients(String carbs, String protein, String fat, String salt, String fibre) {
+        this.carbohydrates = carbs;
+        this.protein = protein;
+        this.fat = fat;
+        this.salt = salt;
+        this.fibre = fibre;
+    }
+
+    public String getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(String carbohydrates) {
+        this.carbs = carbs;
+    }
+
+    public String getProtein() {
+        return protein;
+    }
+
+    public void setProtein(String protein) {
+        this.protein = protein;
+    }
+
+    public String getFat() {
+        return fat;
+    }
+
+    public void setFat(String fat) {
+        this.fat = fat;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getFibre() {
+        return fibre;
+    }
+
+    public void setFibre(String fibre) {
+        this.fibre = fibre;
+    }
+}
