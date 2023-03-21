6837207bdbb282f7235c85b5dc00c08575ee481a
HoXanh <hoxuananh543@gmail.com>
2023-03-21 09:37:00 +0000

    ChangeFoodDataActivity - update ability to delete database food input

--- app/src/main/java/com/TrackManInc/mytracker/ChangeFoodDataActivity.java
+++ app/src/main/java/com/TrackManInc/mytracker/ChangeFoodDataActivity.java
@@ -6,25 +6,34 @@ import com.TrackManInc.mytracker.Prevalent.Prevalent;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.AlignmentSpan;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.*;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import android.widget.Button;
 
 
 public class ChangeFoodDataActivity extends AppCompatActivity {
 
     private String date, foodName;
     private EditText carbAmountET, proteinAmountET, fatsAmountET, saltAmountET, fiberAmountET, foodNameET, dateET, quantityAmountET;
-
+    private Button deleteButton, saveButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,8 +53,46 @@ public class ChangeFoodDataActivity extends AppCompatActivity {
         saltAmountET = findViewById(R.id.editSalt);
         fiberAmountET = findViewById(R.id.editFiber);
         quantityAmountET = findViewById(R.id.quantity);
+        deleteButton = findViewById(R.id.delete_button);
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            //delete Food Item from database
+            @Override
+            public void onClick(View view) {deleteInput();}
+        });
+        saveButton = findViewById(R.id.save_button);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 
+    private void deleteInput(){
+        //Delete from database
+        final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        rootRef.child("User Foods").child(Prevalent.currentOnlineUser.getEmail()).child(date).child(foodName).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    toastMessage("Food item deleted.");
+                    finish();
+                }else{
+                    toastMessage("Network Error: Please try again after some time...");
+                }
+            }
+        });
+    }
+
+    private void toastMessage(String message){
+        Spannable centeredText = new SpannableString(message);
+        centeredText.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER),0,message.length()-1,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        Toast.makeText(getApplicationContext(), centeredText, Toast.LENGTH_SHORT).show();
+    }
 
     private void getData(String formattedDate) {
         final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
